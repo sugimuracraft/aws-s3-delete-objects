286533351b531792da1184f8d188c974847f6996
Kohei Sugimura <sugimuracraft@gmail.com>
2021-03-04 07:27:44 +0900

    modified to pass prameters as options.

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ This is a tool to delete the target files (1,000 or more) from the bucket that s
 add or confirm your credential.
 ```bash
 $ cat << EOF >> ~/.aws/credentials
-[default]
+[example-account]
 aws_access_key_id = AKI00000000000000000
 aws_secret_access_key = 0000000000000000000000000000000000000000
 EOF
@@ -22,14 +22,46 @@ EOF
 $ npm install
 ```
 # Usage
-## configure target bucket and path.
-open index.js, and edit following.
-```js
-const bucket = 'bucket-name'
-const key = 'path/to/delete/prefix';
+
+## delete objects which start with prefix.
+```bash
+$ node index.js \
+    --profile "example-account" \
+    --bucket "bucket-name" \
+    --prefix "path/to/delete/prefix"
 ```
 
-## execute
+## delete objects with object list.
+* list.txt
+```
+path/to/delete/1.txt
+path/to/delete/2.txt
+path/to/delete/3.txt
+dir/to/delete
+```
+
 ```bash
-$ npm start
+$ cat list.txt \
+  | xargs -n 1 \
+      node index.js \
+        --profile "example-account" \
+        --bucket "bucket-name" \
+        --prefix 
+```
+
+## delete versioned objects.
+* list.txt
+```
+path/to/delete/1.txt
+path/to/delete/2.txt
+path/to/delete/3.txt
+dir/to/delete
+```
+
+```bash
+$ node index.js \
+    --profile "example-account" \
+    --bucket "bucket-name" \
+    --versions \
+    --prefix "path/to/delete/prefix"
 ```
--- index.js
+++ index.js
@@ -1,16 +1,124 @@
-console.log('program started.');
-
 const AWS = require('aws-sdk');
+const commandLineArgs = require('command-line-args');
+
+
+console.log('program started.');
+const optionDefinitions = [
+  {
+    name: 'profile',
+    type: String
+  },
+  {
+    name: 'bucket',
+    type: String
+  },
+  {
+    name: 'prefix',
+    type: String
+  },
+  {
+    name: 'versions',
+    type: Boolean
+  }
+];
+const options = commandLineArgs(optionDefinitions);
+if (!options.hasOwnProperty('profile')) {
+  options['profile'] = 'default';
+}
+if (!options.hasOwnProperty('bucket')) {
+  console.error('specify bucket.');
+  process.exit(1);
+}
+if (!options.hasOwnProperty('prefix')) {
+  console.error('specify prefix.');
+  process.exit(1);
+}
+if (!options.hasOwnProperty('versions')) {
+  options['versions'] = false;
+}
+console.log(options);
+
+AWS.config.credentials = new AWS.SharedIniFileCredentials({
+  profile: options['profile']
+});
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
-const bucket = 'bucket-name'
-const key = 'path/to/delete/prefix';
+const bucket = options.bucket;
+const prefix = options.prefix;
 
-let objects = [];
+const maxKeys = 1000;
+let targetObjects = [];
+let nextContinuationToken = '';
 let nextKeyMarker = '';
 let nextVersionIdMarker = '';
 let totalCount = 0;
 
+
+const listObjects = () => {
+  return new Promise((resolve, reject) => {
+
+    const subtask = () => {
+      return new Promise((resolve, reject) => {
+        console.log('-- begin listObjects');
+        if (nextContinuationToken === null) {  // done this task when nextContinuationToken is null.
+          resolve();
+          return;
+        }
+        if (targetObjects.length >= 10000) {
+          console.log('---- queued targetObjects full. idling');
+          // idle 10 seconds.
+          const id = setInterval(() => {
+            clearInterval(id);
+            subtask()
+            .then(resolve)
+            .catch(reject);
+          }, 1000 * 10);
+          return;
+        }
+        const params = {
+          Bucket: bucket, 
+          Prefix: prefix,
+          MaxKeys: maxKeys
+        };
+        if (nextContinuationToken !== '') {
+          params['ContinuationToken'] = nextContinuationToken;
+        }
+        s3.listObjectsV2(params, function(err, data) {
+          if (err) {
+            console.error(err, err.stack);
+            reject();
+            return;
+          }
+          console.log(`-- end listObjects, Count=${data.Contents.length}. NextContinuationToken=${data.NextContinuationToken}`);
+          if (data.Contents.length <= 0) {
+            nextContinuationToken = null;
+            resolve();
+            return;
+          }
+          targetObjects = targetObjects.concat(data.Contents.map((object) => {
+            return {
+              Key: object.Key
+            };
+          }));
+          console.log(`---- queued targetObjects length=${targetObjects.length}`);
+          if (!data.NextContinuationToken) {
+            nextContinuationToken = null;
+          } else {
+            nextContinuationToken = data.NextContinuationToken;
+          }
+          subtask()
+          .then(resolve)
+          .catch(reject);
+        });
+      });
+    }
+    
+    subtask()  // begin first task.
+    .then(resolve)  // resolved when all task done.
+    .catch(reject);
+  
+  });
+}
 
 const listObjectVersions = () => {
   return new Promise((resolve, reject) => {
@@ -22,21 +130,21 @@ const listObjectVersions = () => {
           resolve();
           return;
         }
-        if (objects.length >= 10000) {
-          console.log('---- queued objects full. idling');
+        if (targetObjects.length >= 10000) {
+          console.log('---- queued targetObjects full. idling');
           // idle 10 seconds.
           const id = setInterval(() => {
             clearInterval(id);
             subtask()
-              .then(resolve)
-              .catch(reject);
+            .then(resolve)
+            .catch(reject);
           }, 1000 * 10);
           return;
         }
         const params = {
           Bucket: bucket, 
-          Prefix: key,
-          MaxKeys: 1000
+          Prefix: prefix,
+          MaxKeys: maxKeys
         };
         if (nextVersionIdMarker !== '') {
           params['KeyMarker'] = nextKeyMarker;
@@ -50,16 +158,18 @@ const listObjectVersions = () => {
           else {
             console.log(`-- end listObjectVersions, Count=${data.Versions.length}. NextVersionIdMarker=${data.NextVersionIdMarker}`);
             if (data.Versions.length <= 0) {
+              nextKeyMarker = null;
+              nextVersionIdMarker = null;
               resolve();
               return;
             }
-            objects = objects.concat(data.Versions.map((object) => {
+            targetObjects = targetObjects.concat(data.Versions.map((object) => {
               return {
                 versionId: object.VersionId,
                 lastModified: object.LastModified
               };
             }));
-            console.log(`---- queued object length=${objects.length}`);
+            console.log(`---- queued targetObjects length=${targetObjects.length}`);
             if (!data.NextVersionIdMarker) {
               nextKeyMarker = null;
               nextVersionIdMarker = null;
@@ -68,16 +178,16 @@ const listObjectVersions = () => {
               nextVersionIdMarker = data.NextVersionIdMarker;
             }
             subtask()
-              .then(resolve)
-              .catch(reject);
+            .then(resolve)
+            .catch(reject);
           }
         });
       });
     }
     
     subtask()  // begin first task.
-      .then(resolve)  // resolved when all task done.
-      .catch(reject);
+    .then(resolve)  // resolved when all task done.
+    .catch(reject);
   
   });
 }
@@ -88,31 +198,38 @@ const deleteObjects = () => {
     const subtask = () => {
       return new Promise((resolve, reject) => {
         console.log('-- begin deleteObjects');
-        if (nextVersionIdMarker === null && objects.length <= 0) {
+        if (nextContinuationToken === null
+            && nextVersionIdMarker === null
+            && targetObjects.length <= 0) {
           console.log('delete completed');
           resolve();
           return;
         }
-        if (objects.length <= 0) {
-          console.log('---- queued objects empty');
-          // idle 10 seconds.
+        if (targetObjects.length <= 0) {
+          console.log('---- queued targetObjects empty');
+          // idle some seconds.
           const id = setInterval(() => {
             clearInterval(id);
             subtask()
               .then(resolve)
               .catch(reject);
-          }, 1000 * 10);
+          }, 1000 * 2);
           return;
         }
-        const versions = objects.splice(0, 1000);
+        const objects = targetObjects.splice(0, 1000);
         const params = {
           Bucket: bucket, 
           Delete: {
-            Objects: versions.map((version) => {
-              return {
-                Key: key,
-                VersionId: version.versionId
+            Objects: objects.map((object) => {
+              if (!object.hasOwnProperty('VersionId')) {
+                return {
+                  Key: object.Key
+                };
               }
+              return {
+                Key: object.Key,
+                VersionId: object.VersionId
+              };
             }),
             Quiet: false
           }
@@ -123,8 +240,8 @@ const deleteObjects = () => {
             reject();
           }
           else {
-            totalCount += versions.length;
-            console.log(`LastDeleted LastModified: ${versions[versions.length - 1].lastModified}, totalCount=${totalCount}`);
+            totalCount += objects.length;
+            console.log(`LastDeleted LastModified: ${objects[objects.length - 1].lastModified}, totalCount=${totalCount}`);
             subtask()
               .then(resolve)
               .catch(reject);
@@ -134,13 +251,26 @@ const deleteObjects = () => {
     };
 
     subtask()  // begin first task.
-      .then(resolve)  // resolved when all task done.
-      .catch(reject);
+    .then(resolve)  // resolved when all task done.
+    .catch(reject);
 
   });
 }
 
-Promise.all([listObjectVersions(), deleteObjects()])
-  .then(() => {
-    console.log('completed.');
-  });
+let listTask;
+if (options.versions) {
+  listTask = listObjectVersions;
+  // force complete about listObjects.
+  nextContinuationToken = null;
+}
+else {
+  listTask = listObjects;
+  // force complete about listObjectVersions.
+  nextKeyMarker = null;
+  nextVersionIdMarker = null;
+}
+Promise
+.all([listTask(), deleteObjects()])
+.then(() => {
+  console.log('completed.');
+});
